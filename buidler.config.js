90c22f8ebe4d9c2c699446e13dd3691c8d37d258
Matt Solomon <matt@mattsolomon.dev>
2020-08-17 18:56:52 -0700

    Run Prettier

--- buidler.config.js
+++ buidler.config.js
@@ -4,7 +4,7 @@ usePlugin("@nomiclabs/buidler-etherscan");
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
+task('accounts', 'Prints the list of accounts', async () => {
   const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
@@ -25,7 +25,7 @@ module.exports = {
   },
   networks: {
     localhost: {
-      url: "http://localhost:8545",
+      url: 'http://localhost:8545',
       accounts: {
         mnemonic: process.env.MNEMONIC_RINKEBY,
       },
--- contracts/test/MockDAI.sol
+++ contracts/test/MockDAI.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 
 contract MockDAI is ERC20UpgradeSafe {
-    constructor() public {
-      ERC20UpgradeSafe.__ERC20_init("MockDAI", "DAI");
-    }
+  constructor() public {
+    ERC20UpgradeSafe.__ERC20_init("MockDAI", "DAI");
+  }
 
-    function faucet() public {
-      _mint(msg.sender, 10**18);
-    }
+  function faucet() public {
+    _mint(msg.sender, 10**18);
+  }
 }
